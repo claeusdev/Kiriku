cd42b0ce5c24d7b125e01dc649015c6cf8f70955
Nana Ewusi <mrewusi@gmail.com>
2018-04-14 21:59:41 +0000

    Redirect / to /admin

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -3,10 +3,8 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 import { Provider } from 'react-redux';
 
-import { NotFound } from '../pages/NotFound';
 import AuthenticatedRoute from '../AuthenticatedRoute/Container';
 import UnauthenticatedRoute from '../UnauthenticatedRoute/Container';
-import Login from '../pages/Login/Container'; 
 import Admin from '../screens/Admin/Container'; 
 
 const Root = ({ store, history }) => {
@@ -15,9 +13,11 @@ const Root = ({ store, history }) => {
       <Provider store={store}>
         <ConnectedRouter history={history}>
           <Switch>
-            <UnauthenticatedRoute exact path="/" component={Login}/>
+            <Route exact path="/" render={(props) => {
+              return <Redirect to='/admin' />
+            }}/>
             <Route path="/admin" component={Admin}/>
-            <Route component={NotFound}/>
+            <Route component={() => <p>That page doesn't exist</p>}/>
           </Switch>
         </ConnectedRouter>
       </Provider>
