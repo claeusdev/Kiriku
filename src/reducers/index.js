e2b5c2a1f100b717625cdc1eeff24e924cd79848
Nana Ewusi <mrewusi@gmail.com>
2018-04-15 16:54:52 +0000

    Add dummy reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,17 @@
 import { combineReducers } from 'redux';
 import createQuizPage from './createQuizPage';
 
+const on = (state = false, action) => {
+  switch (action.type) {
+    case 'SET_IS_SAVING_QUIZ':
+      return action.value;
+    default:
+      return state;
+  }
+}
+
 const reducer = combineReducers({
+  on
 });
 
 export default reducer;
