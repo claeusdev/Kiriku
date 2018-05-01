7b8b63dfd7a69a6bd58848efeae07a9bcb43f31d
Nana Ewusi <mrewusi@gmail.com>
2018-05-01 22:47:33 +0000

    Add  path

--- src/components/screens/Admin/Collections/index.js
+++ src/components/screens/Admin/Collections/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Article from './Article';
+import Gif from './Gif';
 
 export default () => {
 	return (
 		<div className="LunaAdmin-Content-Collections">
 			<Switch>
 				<Route path="/admin/collections/article" component={Article} />
+				<Route path="/admin/collections/gif" component={Gif} />
 			</Switch>
 		</div>
 	);
