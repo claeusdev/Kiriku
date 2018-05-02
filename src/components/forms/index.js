6293f0b8afb9165d8cf9ccec7a66fd56a1a51cef
Nana Ewusi <mrewusi@gmail.com>
2018-05-02 12:50:41 +0000

    Add for to update gifs

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -1,5 +1,11 @@
 import NewArticleForm from './NewArticle';
 import UpdateArticleForm from './UpdateArticle';
 import NewGifForm from './NewGif';
+import UpdateGifForm from './UpdateGif';
 
-export { NewArticleForm, UpdateArticleForm, NewGifForm };
+export { 
+	NewArticleForm, 
+	UpdateArticleForm, 
+	NewGifForm,
+	UpdateGifForm
+};
